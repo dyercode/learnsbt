386a57726a31c03faab8fc783261881cf84c2cc4
Jon <dyercode@users.noreply.github.com>
2018-04-22 18:45:13 -0400

    update dependencies and scala and sbt versions.

--- build.sbt
+++ build.sbt
@@ -4,6 +4,6 @@ name := "learnsbt"
 
 version := "1.0"
 
-scalaVersion := "2.11.4"
+scalaVersion := "2.12.4"
 
 libraryDependencies ++= learnSbtDependencies
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object Dependencies {
-    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.2"
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
 
     val learnSbtDependencies = Seq(scalaTest % "test")
 }
